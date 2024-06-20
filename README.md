a7b5942d5c3d9c207b4eb42eceb22b584955b9e6
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2024-06-20 19:37:21 +0000

    feat: 🎉 init project

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# aws-lambda-profile
-aws-lambda-profile
+# profile: AWS Lambda performance profiling
+
+![Go Version](https://img.shields.io/badge/Go-%3E%3D%201.21-%23007d9c)
+[![GoDoc](https://godoc.org/github.com/jbleduigou/aws-lambda-profile?status.svg)](https://pkg.go.dev/github.com/jbleduigou/aws-lambda-profile)
+![Build Status](https://github.com/jbleduigou/aws-lambda-profile/actions/workflows/go.yml/badge.svg)
+[![Go report](https://goreportcard.com/badge/github.com/jbleduigou/aws-lambda-profile)](https://goreportcard.com/report/github.com/jbleduigou/aws-lambda-profile)
+[![Contributors](https://img.shields.io/github/contributors/jbleduigou/aws-lambda-profile)](https://github.com/jbleduigou/aws-lambda-profile/graphs/contributors)
+[![License](https://img.shields.io/github/license/jbleduigou/aws-lambda-profile)](./LICENSE)
+
+An [AWS Lambda Function](https://aws.amazon.com/lambda/) performance profiling tool based on [profile package](https://github.com/pkg/profile) by [Dave Cheney](https://github.com/davecheney).  
+The idea is to provide an adapter on top of the existing package to make it easier to use in the context of AWS Lambda.  
+The profiling output file is uploaded to an S3 bucket.
+
+## 🚀 Install
+
+```sh
+go get github.com/jbleduigou/aws-lambda-profile
+```
+
+**Compatibility**: go >= 1.21
+
+
+## 💡 Usage
+
+GoDoc: [https://pkg.go.dev/github.com/jbleduigou/aws-lambda-profile](https://pkg.go.dev/github.com/jbleduigou/aws-lambda-profile)
+
+### Basic example
+
+To enable profiling in your application, you need to add one line in your main function:
+
+```go
+import profile "github.com/jbleduigou/aws-lambda-profile"
+
+func main() {
+    defer profile.Start(profile.S3Bucket("pprof-bucket"), profile.AWSRegion("eu-west-1")).Stop()
+}
+```
+
+### CPU profiling
+
+By default, the CPU profiling is enabled.  
+You can still explicitly enable it by using the `CPUProfile` option:
+
+```go
+import profile "github.com/jbleduigou/aws-lambda-profile"
+
+defer profile.Start(profile.CPUProfile, profile.S3Bucket("pprof-bucket"), profile.AWSRegion("eu-west-1")).Stop()
+```
+
+### Memory profiling
+
+To enable memory profiling, you can use the `MemProfile` option:
+
+```go
+import profile "github.com/jbleduigou/aws-lambda-profile"
+
+defer profile.Start(profile.MemProfile, profile.S3Bucket("pprof-bucket"), profile.AWSRegion("eu-west-1")).Stop()
+```
+
+
+
+## 🤝 Contributing
+
+Feel free to contribute to this project, either my opening issues or submitting pull requests.  
+Don't hesitate to contact me, by sending me a PM on [LinkedIn](www.linkedin.com/in/jbleduigou).
+
+## 👤 Contributors
+
+The only contributor so far is me, Jean-Baptiste Le Duigou.
+Feel free to check my blog to about my other projects: http://www.jbleduigou.com
+
+## 💫 Show your support
+
+Give a ⭐️ if this project helped you!
+
+[![GitHub Sponsors](https://img.shields.io/github/sponsors/jbleduigou?style=for-the-badge)](https://github.com/sponsors/jbleduigou)
+
+## 📝 License
+
+This project is [BSD](./LICENSE) licensed.
---
+++ profile.go
@@ -0,0 +1,142 @@
+package profile
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/lambdacontext"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+
+	pkgprofile "github.com/pkg/profile"
+)
+
+// Profile is an adapter for github.com/pkg/profile
+type Profile struct {
+	// bucket is the name of the S3 bucket to upload the profile to.
+	bucket string
+
+	// region is the AWS region of the S3 bucket.
+	region string
+
+	// localPath is the path to the local file where the profile is stored.
+	localPath string
+
+	// p is the actual profile object from github.com/pkg/profile
+	p interface{ Stop() }
+}
+
+// Quiet suppresses informational messages during profiling.
+func Quiet(*Profile) func(pkg *pkgprofile.Profile) {
+	return pkgprofile.Quiet
+}
+
+// CPUProfile enables CPU profiling.
+// It disables any previous profiling settings.
+func CPUProfile(p *Profile) func(pkg *pkgprofile.Profile) {
+	p.localPath = "/tmp/cpu.pprof"
+	return pkgprofile.CPUProfile
+}
+
+// MemProfile enables memory profiling.
+// It disables any previous profiling settings.
+func MemProfile(p *Profile) func(pkg *pkgprofile.Profile) {
+	p.localPath = "/tmp/mem.pprof"
+	return pkgprofile.MemProfile
+}
+
+// S3Bucket sets the name of the S3 bucket to upload the profile to.
+func S3Bucket(bucket string) func(*Profile) func(pkg *pkgprofile.Profile) {
+	return func(p *Profile) func(pkg *pkgprofile.Profile) {
+		p.bucket = bucket
+		return nil
+	}
+}
+
+// AWSRegion sets the AWS region of the S3 bucket.
+func AWSRegion(region string) func(*Profile) func(pkg *pkgprofile.Profile) {
+	return func(p *Profile) func(pkg *pkgprofile.Profile) {
+		p.region = region
+		return nil
+	}
+}
+
+// Stop stops the profile and flushes any unwritten data.
+func (p *Profile) Stop() {
+	p.p.Stop()
+	p.uploadToS3()
+}
+
+// uploadToS3 uploads the profile to S3 if the bucket name is provided.
+func (p *Profile) uploadToS3() {
+	if p.bucket == "" {
+		log.Fatalf("bucket name is not provided, skipping upload to S3")
+		return
+	}
+
+	file, err := os.Open(p.localPath)
+	if err != nil {
+		log.Fatalf("unable to open file %s, %v", p.localPath, err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatalf("unable to close file, %v", err)
+		}
+	}(file)
+
+	s3Path := p.generateS3Path()
+
+	sdkConfig, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(p.region))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+	s3Client := s3.NewFromConfig(sdkConfig)
+	_, err = s3Client.PutObject(context.Background(), &s3.PutObjectInput{
+		Bucket: aws.String(p.bucket),
+		Key:    aws.String(s3Path),
+		Body:   file,
+	})
+	if err != nil {
+		log.Fatalf("unable to upload file, %v", err)
+	}
+}
+
+// generateS3Path generates the S3 path based on the lambda context and the current local path.
+func (p *Profile) generateS3Path() string {
+	// retrieve lambda context
+	lc, found := lambdacontext.FromContext(context.Background())
+	if !found {
+		return strings.Replace(p.localPath, "/tmp/", "unknown/", 1)
+	}
+	return strings.Replace(p.localPath, "/tmp/", fmt.Sprintf("%s/%s/", lc.InvokedFunctionArn, lc.AwsRequestID), 1)
+}
+
+// Start starts a new profiling session.
+// The caller should call the Stop method on the value returned
+// to cleanly stop profiling.
+func Start(options ...func(*Profile) func(pkg *pkgprofile.Profile)) interface {
+	Stop()
+} {
+
+	var pkgOptions []func(*pkgprofile.Profile)
+	var profile Profile
+	for _, option := range options {
+		o := option(&profile)
+		if o != nil {
+			pkgOptions = append(pkgOptions, o)
+		}
+	}
+	pkgOptions = append(pkgOptions, pkgprofile.ProfilePath("/tmp"))
+
+	if profile.region == "" {
+		profile.region = "us-east-1"
+	}
+
+	profile.p = pkgprofile.Start(pkgOptions...)
+	return &profile
+}
